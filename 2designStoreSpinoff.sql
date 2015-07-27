0b42cc180421056cefd3288fe390f226b40c24f3
suranand <suranands@gmail.com>
2015-07-27 13:48:58 +0000

    Just started learning sql

---
+++ 2designStoreSpinoff.sql
@@ -0,0 +1,24 @@
+/* Design Book Store Spin Off Project */
+
+CREATE TABLE bookstore (id INTEGER PRIMARY KEY, item TEXT, copies_sold_in_year INTEGER, price INTEGER, copies_avlbl INTEGER);
+INSERT INTO bookstore VALUES (1, "Brief History of Time", 756, 34, 19);
+INSERT INTO bookstore VALUES (2, "Tuesdays with Morries", 823, 33, 12);
+INSERT INTO bookstore VALUES (3, "Physics Teaching", 2, 84, 3);
+INSERT INTO bookstore VALUES (4, "Quantum Mechanics", 1135, 55, 230);
+INSERT INTO bookstore VALUES (5, "NGS Data Analysis", 1357, 67, 343);
+INSERT INTO bookstore VALUES (6, "Hadoop and Genomics", 533, 24, 247);
+INSERT INTO bookstore VALUES (7, "Big Data 2015", 987, 37, 439);
+INSERT INTO bookstore VALUES (8, "Dooms Day", 126, 7, 58);
+INSERT INTO bookstore VALUES (9, "DaVinci Code", 1536, 39, 139);
+INSERT INTO bookstore VALUES (10, "Bla Bla Bla", 120, 3, 10);
+INSERT INTO bookstore VALUES (11, "Do Re Ga Ma", 89, 2.5, 5);
+INSERT INTO bookstore VALUES (12, "Vedas", 197, 19, 200);
+INSERT INTO bookstore VALUES (13, "Yoga", 97, 5, 100);
+INSERT INTO bookstore VALUES (14, "Philosophy of Physics", 273, 11, 150);
+INSERT INTO bookstore VALUES (15, "Android SDK", 697, 21, 395);
+
+SELECT * FROM bookstore;
+SELECT * FROM bookstore ORDER BY price;
+SELECT item, copies_sold_in_year FROM bookstore;
+SELECT item, copies_sold_in_year FROM bookstore WHERE copies_sold_in_year > 500;
+SELECT SUM(copies_avlbl) FROM bookstore;
---
+++ README.md
@@ -0,0 +1,17 @@
+# Learn SQL
+
+### Today, July 27, 2015
+
+This is my attempt to learn SQL using [Khan Academy SQL Course](https://www.khanacademy.org/computing/sql) and the repo contains all the SQL scripts worked out suring the course projects and spin-offs.
+
+The `solar_system_objects` dataset is taken from [here](https://gist.github.com/pamelafox/30acd9ca938c8a62fb92).
+
+Out of the 4 sections in the course, 2 are completed. 
+
+- SQL Basics
+- More Advanced SQL Queries
+- Relational Queries in SQL
+- Modifying Databases with SQL
+- *Further Learning in SQL*
+
+Eager to finish this at the earliest!
